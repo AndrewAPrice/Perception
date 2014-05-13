80405926a952f864036c3a2ebf7d1d2c683fe398
MessiahAndrw <messiahandrw@gmail.com>
2014-05-13 11:14:38 -0500

    added fast mod, other headers, closures, function pointers, objects, properties

--- turkey/turkey_internal.h
+++ turkey/turkey_internal.h
@@ -2,6 +2,12 @@
 #define TURKEY_INTERNAL_H
 #include "turkey.h"
 
+#ifdef DEBUG
+#include <assert.h>
+#else
+#define assert(v)
+#endif
+
 struct TurkeyGarbageCollectedObject {
 	bool marked; /* has this item been marked for sweeping? */
 	unsigned int hold; /* don't garbage collect if true */
@@ -10,8 +16,10 @@ struct TurkeyGarbageCollectedObject {
 };
 
 struct TurkeyClosure {
-	TurkeyClosure *parent;
-	unsigned long long int references;
+	TurkeyClosure *parent; /* parent closure, look in here if it's not found in this closure */
+	unsigned int count; /* number of variables */
+	unsigned int references; /* references to this closure - closures are referenced counted, not by the garbage collector but must be deleted it falls to 0 */
+	TurkeyVariable *variables; /* array of variables*/
 };
 
 struct TurkeyString : TurkeyGarbageCollectedObject {
@@ -30,8 +38,8 @@ struct TurkeyStringTable {
 };
 
 struct TurkeyFunctionPointer : TurkeyGarbageCollectedObject {
-	TurkeyFunction *function;
-	TurkeyClosure *closure;
+	TurkeyFunction *function; /* the function to call */
+	TurkeyClosure *closure; /* the closure */
 };
 
 struct TurkeyArray : TurkeyGarbageCollectedObject {
@@ -47,7 +55,16 @@ struct TurkeyBuffer : TurkeyGarbageCollectedObject {
 	size_t size; /* length of the buffer */
 };
 
+struct TurkeyObjectProperty {
+	TurkeyString *key; /* key identifying the property */
+	TurkeyObjectProperty *next; /* pointer to the next property at this chain */
+	TurkeyVariable value; /* the value */
+};
+
 struct TurkeyObject : TurkeyGarbageCollectedObject {
+	unsigned int count; /* the number of properties */
+	unsigned int size; /* size of the hash table */
+	TurkeyObjectProperty **properties; /* hash table of properties */
 };
 
 struct TurkeyFunction {
@@ -125,7 +142,7 @@ struct TurkeyVM {
 	unsigned long long int current_byteindex; /* index of the currently executing bytecode */
 };
 
-/* array.h */
+/* array.cpp */
 extern TurkeyArray *turkey_array_new(TurkeyVM *vm, unsigned int size);
 extern void turkey_array_delete(TurkeyVM *vm, TurkeyArray *arr);
 extern void turkey_array_push(TurkeyVM *vm, TurkeyArray *arr, const TurkeyVariable &variable);
@@ -138,7 +155,7 @@ extern void turkey_array_remove(TurkeyVM *vm, TurkeyArray *arr, unsigned int sta
 extern TurkeyArray *turkey_array_splice(TurkeyVM *vm, TurkeyArray *arr, unsigned int start, unsigned int count);
 extern void turkey_array_insert(TurkeyVM *vm, TurkeyArray *arr, unsigned int index, const TurkeyVariable &variable);
 
-/* buffer.h */
+/* buffer.cpp */
 extern TurkeyBuffer *turkey_buffer_new(TurkeyVM *vm, size_t size);
 extern TurkeyBuffer *turkey_buffer_new_native(TurkeyVM *vm, void *ptr, size_t size);
 extern void turkey_buffer_delete(TurkeyVM *vm, TurkeyBuffer *buffer);
@@ -167,15 +184,21 @@ extern double turkey_buffer_read_float_16(TurkeyVM *vm, TurkeyBuffer *buffer, un
 extern double turkey_buffer_read_float_32(TurkeyVM *vm, TurkeyBuffer *buffer, unsigned long long int address);
 extern double turkey_buffer_read_float_64(TurkeyVM *vm, TurkeyBuffer *buffer, unsigned long long int address);
 
-/* callstack.h */
+/* callstack.cpp */
 extern void turkey_callstack_init(TurkeyCallStack &stack);
 extern void turkey_callstack_cleanup(TurkeyCallStack &stack);
-/* adds and returns the entry (to write into) */
 extern TurkeyCallStackEntry *turkey_callstack_push(TurkeyCallStack &stack);
-/* pops an element and returns the entry */
 extern TurkeyCallStackEntry *turkey_callstack_pop(TurkeyCallStack &stack);
 
-/* gc.h */
+/* closure.cpp */
+extern TurkeyClosure *turkey_closure_create(TurkeyVM *vm, TurkeyClosure *parent, unsigned int variables);
+extern void turkey_closure_delete(TurkeyVM *vm, TurkeyClosure *closure);
+
+/* functionpointer.cpp */
+extern TurkeyFunctionPointer *turkey_functionpointer_new(TurkeyVM *vm, TurkeyFunction *function, TurkeyClosure *closure);
+extern void turkey_functionpointer_delete(TurkeyVM *vm, TurkeyFunctionPointer *funcptr);
+
+/* gc.cpp */
 extern void turkey_gc_init(TurkeyGarbageCollector &collector);
 extern void turkey_gc_cleanup(TurkeyGarbageCollector &collector);
 extern void turkey_gc_register_string(TurkeyGarbageCollector &collector, TurkeyString *string);
@@ -184,7 +207,15 @@ extern void turkey_gc_register_array(TurkeyGarbageCollector &collector, TurkeyAr
 extern void turkey_gc_register_object(TurkeyGarbageCollector &collector, TurkeyObject *object);
 extern void turkey_gc_register_function_pointer(TurkeyGarbageCollector &collector, TurkeyFunctionPointer *closure);
 
-/* stack.h */
+/* object.cpp */
+extern TurkeyObject *turkey_object_new(TurkeyVM *vm);
+extern void turkey_object_delete(TurkeyVM *vm, TurkeyObject *object);
+extern TurkeyVariable turkey_object_get_property(TurkeyVM *vm, TurkeyObject *object, TurkeyString *name);
+extern void turkey_object_set_property(TurkeyVM *vm, TurkeyObject *object, TurkeyString *name, const TurkeyVariable &value);
+extern void turkey_object_grow(TurkeyVM *vm, TurkeyObject *object);
+extern void turkey_object_delete_property(TurkeyVM *vm, TurkeyObject *object, TurkeyString *name);
+
+/* stack.cpp */
 extern void turkey_stack_init(TurkeyStack &stack);
 extern void turkey_stack_cleanup(TurkeyStack &stack);
 extern void turkey_stack_push(TurkeyStack &stack, const TurkeyVariable &value);
@@ -193,11 +224,20 @@ extern void turkey_stack_pop(TurkeyStack &stack, TurkeyVariable &value);
 extern void turkey_get(TurkeyStack &stack, unsigned int position, TurkeyVariable &value);
 extern void turkey_set(TurkeyStack &stack, unsigned int position, const TurkeyVariable &value);
 
-/* stringtable.h */
+/* stringtable.cpp */
 extern void turkey_stringtable_init(TurkeyStringTable &table);
 extern void turkey_stringtable_cleanup(TurkeyStringTable &table);
 extern TurkeyString *turkey_stringtable_newstring(TurkeyStringTable &table, const char *string, unsigned int length);
-extern void turkey_stringtable_resize(TurkeyStringTable &table);
+extern void turkey_stringtable_grow(TurkeyStringTable &table);
 extern void turkey_stringtable_removestring(TurkeyStringTable &table, TurkeyString *string);
+
+/* utilities */
+/* fast hash if size is a power of two */
+inline unsigned int fast_mod(unsigned int hash, unsigned int size) {
+	/* size be a power of two */
+	assert((size & (size - 1)) == 0);
+
+	return hash & (size - 1);
+}
 
 #endif
