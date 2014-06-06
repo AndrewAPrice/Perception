9f92e662e94444d735e4b4c0ac9dae0551fedee3
MessiahAndrw <messiahandrw@gmail.com>
2014-06-06 10:09:22 -0500

    added native functions

--- turkey/functionpointer.cpp
+++ turkey/functionpointer.cpp
@@ -5,17 +5,29 @@ TurkeyFunctionPointer *turkey_functionpointer_new(TurkeyVM *vm, TurkeyFunction *
 	closure->references++;
 
 	TurkeyFunctionPointer *funcptr = (TurkeyFunctionPointer *)turkey_allocate_memory(sizeof TurkeyFunctionPointer);
-	funcptr->function = function;
-	funcptr->closure = closure;
+	funcptr->is_native = false;
+	funcptr->managed.function = function;
+	funcptr->managed.closure = closure;
+
+	return funcptr;
+}
+
+TurkeyFunctionPointer *turkey_functionpointer_new_native(TurkeyVM *vm, TurkeyNativeFunction function, void *closure) {
+	TurkeyFunctionPointer *funcptr = (TurkeyFunctionPointer *)turkey_allocate_memory(sizeof TurkeyFunctionPointer);
+	funcptr->is_native = true;
+	funcptr->native.function = function;
+	funcptr->native.closure = closure;
 
 	return funcptr;
 }
 
 void turkey_functionpointer_delete(TurkeyVM *vm, TurkeyFunctionPointer *funcptr) {
-	/* dereference the closure */
-	funcptr->closure->references--;
-	if(funcptr->closure->references == 0)
-		turkey_closure_delete(vm, funcptr->closure);
+	if(!funcptr->is_native) {
+		/* dereference the closure */
+		funcptr->managed.closure->references--;
+		if(funcptr->managed.closure->references == 0)
+			turkey_closure_delete(vm, funcptr->managed.closure);
+	}
 
 	turkey_free_memory(funcptr);
 }
