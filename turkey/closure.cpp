39d6e994b4b2461a8b79daf07a593b476c0c9b1c
MessiahAndrw <messiahandrw@gmail.com>
2014-06-09 13:43:14 -0500

    added closure get,set, fixed bugs

--- turkey/closure.cpp
+++ turkey/closure.cpp
@@ -7,6 +7,8 @@ TurkeyClosure *turkey_closure_create(TurkeyVM *vm, TurkeyClosure *parent, unsign
 	closure->count = variables;
 	closure->parent = parent;
 	closure->variables = (TurkeyVariable *)turkey_allocate_memory((sizeof TurkeyVariable) * variables);
+	for(unsigned int i = 0; i < variables; i++)
+		closure->variables[i].type = TT_Null;
 
 	return closure;
 }
@@ -24,4 +26,35 @@ void turkey_closure_delete(TurkeyVM *vm, TurkeyClosure *closure) {
 	}
 
 	turkey_free_memory(closure);
+}
+
+void turkey_closure_get(TurkeyVM *vm, unsigned int position, TurkeyVariable &value) {
+	TurkeyClosure *closure = vm->interpreter_state->closure;
+
+	while(closure != 0) {
+		if(position < closure->count) {
+			value = closure->variables[position];
+			return;
+		}
+
+		position -= closure->count;
+		closure = closure->parent;
+	}
+
+	// not found
+	value.type = TT_Null;
+}
+
+void turkey_closure_set(TurkeyVM *vm, unsigned int position, const TurkeyVariable &value) {
+	TurkeyClosure *closure = vm->interpreter_state->closure;
+
+	while(closure != 0) {
+		if(position < closure->count) {
+			closure->variables[position] = value;
+			return;
+		}
+
+		position -= closure->count;
+		closure = closure->parent;
+	}
 }
