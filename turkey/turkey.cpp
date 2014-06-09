84faea3db2774b97a8efbf5653bb978df3fa295f
MessiahAndrw <messiahandrw@gmail.com>
2014-06-09 13:44:42 -0500

    get rid of call_stack and add interpreter init/cleanup

--- turkey/turkey.cpp
+++ turkey/turkey.cpp
@@ -5,26 +5,26 @@ TurkeyVM *turkey_init(TurkeySettings *settings) {
 	TurkeyVM *vm = (TurkeyVM *)turkey_allocate_memory(sizeof TurkeyVM);
 	vm->debug = settings->debug;
 
-	vm->current_byteindex = 0;
-	vm->current_function_index = 0;
-
+	turkey_interpreter_init(vm);
 	turkey_module_init(vm);
 	turkey_stack_init(vm->local_stack);
 	turkey_stack_init(vm->variable_stack);
-	turkey_callstack_init(vm->call_stack);
+	turkey_stack_init(vm->parameter_stack);
 
-	turkey_stringtable_init(vm->string_table);
+	turkey_stringtable_init(vm);
 
 	return vm;
 };
 
 void turkey_cleanup(TurkeyVM *vm) {
-	turkey_stringtable_cleanup(vm->string_table);
+	turkey_stringtable_cleanup(vm);
 	turkey_stack_cleanup(vm->local_stack);
 	turkey_stack_cleanup(vm->variable_stack);
-	turkey_callstack_cleanup(vm->call_stack);
+	turkey_stack_cleanup(vm->parameter_stack);
 
 	turkey_module_cleanup(vm);
+
+	turkey_interpreter_cleanup(vm);
 	
 	turkey_free_memory(vm);
 };
