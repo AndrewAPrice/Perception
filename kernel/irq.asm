74550ef6ed02443ec4741d487f5814caa1f92b26
Andrew Price <messiahandrw@gmail.com>
2014-12-11 09:19:29 -0600

    fixed indents

--- kernel/irq.asm
+++ kernel/irq.asm
@@ -119,17 +119,17 @@ irq_common_stub:
 	push rbx
 	push rcx
 	push rdx
-    push rsi
-    push rdi
-    push rbp
-    push r8
-    push r9
-    push r10
-    push r11
-    push r12
-    push r13
-    push r14
-    push r15
+	push rsi
+	push rdi
+	push rbp
+	push r8
+	push r9
+	push r10
+	push r11
+	push r12
+	push r13
+	push r14
+	push r15
 
 	mov ax, 0x8
 	mov ds, ax
@@ -142,20 +142,20 @@ irq_common_stub:
 	call rax
 	mov rsp, rax ; returns a pointer to the stack
 
-    pop r15
-    pop r14
-    pop r13
-    pop r12
-    pop r11
-    pop r10
-    pop r9
-    pop r8
-    pop rbp
-    pop rdi
-    pop rsi
-    pop rdx
-    pop rcx
-    pop rbx
-    pop rax
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	pop rbp
+	pop rdi
+	pop rsi
+	pop rdx
+	pop rcx
+	pop rbx
+	pop rax
 	add rsp, 16 ; Clean up the pushed error code and pushed ISR number
 	iretq ; pops RIP, CS, EFLAGS, RSP, SS
