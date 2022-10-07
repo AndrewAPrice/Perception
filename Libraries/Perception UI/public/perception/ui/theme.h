cddc2ee672b2bb55fa02639fd2c40c19493dc9e4
AndrewAPrice <andrewaprice@google.com>
2022-10-07 18:05:24 -0400

    Make colors BGRA instead of RGBA to match what Skia uses under the hood.

--- Libraries/Perception UI/public/perception/ui/theme.h
+++ Libraries/Perception UI/public/perception/ui/theme.h
@@ -12,23 +12,28 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "include/core/SkColor.h"
 #include "types.h"
 
 namespace perception {
 namespace ui {
 
-constexpr uint32 kBackgroundWindowColor = 0xc9fbffff;
-constexpr uint32 kLabelTextColor = 0x000000ff;
-constexpr uint32 kButtonBackgroundColor = 0x8cb7bfff;
-constexpr uint32 kButtonPushedBackgroundColor = 0x779da3ff;
-constexpr uint32 kButtonBrightColor = 0xb1e6f0ff;
-constexpr uint32 kButtonDarkColor = 0x5f7d82ff;
+constexpr uint32 kBackgroundWindowColor =
+    SkColorSetARGB(0xff, 0xc9, 0xfb, 0xff);
+constexpr uint32 kLabelTextColor = SkColorSetARGB(0xff, 0x00, 0x00, 0x00);
+constexpr uint32 kButtonBackgroundColor =
+	SkColorSetARGB(0xff, 0x8c, 0xb7, 0xbf);
+constexpr uint32 kButtonPushedBackgroundColor =
+    SkColorSetARGB(0xff, 0x77, 0x9d, 0xa3);
+constexpr uint32 kButtonBrightColor = SkColorSetARGB(0xff, 0xb1, 0xe6, 0xf0);
+constexpr uint32 kButtonDarkColor = SkColorSetARGB(0xff, 0x5f, 0x7d, 0x82);
 constexpr uint32 kButtonTextColor = kLabelTextColor;
 constexpr uint32 kTextBoxBackgroundColor = kButtonBackgroundColor;
 constexpr uint32 kTextBoxTopLeftOutlineColor = kButtonDarkColor;
 constexpr uint32 kTextBoxBottomRightOutlineColor = kButtonBrightColor;
 constexpr uint32 kTextBoxTextColor = kLabelTextColor;
-constexpr uint32 kTextBoxNonEditableTextColor = 0x111111ff;
+constexpr uint32 kTextBoxNonEditableTextColor =
+    SkColorSetARGB(0xff, 0x11, 0x11, 0x11);
 
 }  // namespace ui
 }  // namespace perception
