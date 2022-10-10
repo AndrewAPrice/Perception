5fc4f9ff0dba3c46d4a4e86b359c964f8af021ac
AndrewAPrice <andrewaprice@google.com>
2022-10-10 09:12:06 -0400

    Adds Adobe DNG SDK

---
+++ Libraries/Adobe DNG SDK/prepare.js
@@ -0,0 +1,134 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const fs = require('fs');
+const child_process = require('child_process');
+const path = require('path');
+const process = require('process');
+
+
+function createDirectoryIfItDoesntExist(dirPath) {
+	if (fs.existsSync(dirPath)) {
+		return;
+	}
+	// Make sure parent exists.
+	const parentPath = path.dirname(dirPath);
+	createDirectoryIfItDoesntExist(parentPath);
+	fs.mkdirSync(dirPath);
+}
+createDirectoryIfItDoesntExist('third_party');
+
+const downloadUrl = 'https://download.adobe.com/pub/adobe/dng/dng_sdk_1_6.zip';
+
+// Check that the third directory exists.
+if (!fs.existsSync('third_party/download.zip')) {
+	console.log('Downloading Adobe DNG SDK');
+	// Grab it from github.
+	let command = 'curl ' + downloadUrl + ' --output third_party/download.zip';
+	try {
+		child_process.execSync(command, {stdio: 'inherit'});
+	} catch (exp) {
+		console.log('Error downloading Adobe DNK SDK: ' + exp);
+		process.exit(1);
+	}
+}
+
+if (!fs.existsSync('third_party/extracted')) {
+	command = 'unzip third_party/download -d third_party/extracted';
+	try {
+		child_process.execSync(command, {stdio: 'inherit'});
+	} catch (exp) {
+		console.log('Error downloading Adobe DNK SDK: ' + exp);
+		process.exit(1);
+	}
+}
+
+// Keep track of third party files we copied from our last build, so we can remove any
+// that no longer exist.
+let third_party_files_from_last_run = {};
+if (fs.existsSync('third_party_files.json')) {
+	third_party_files_from_last_run = JSON.parse(fs.readFileSync('third_party_files.json'));
+}
+let third_party_files = {};
+
+
+function removeDirectoryIfEmpty(dirPath) {
+	if (fs.readdirSync(dirPath).length >= 1) {
+		// There are files in this directory.
+		return;
+	}
+
+	fs.rmdirSync(dirPath);
+	const parentPath = path.dirname(dirPath);
+	removeDirectoryIfEmpty(parentPath);
+}
+
+function copyFile(fromPath, toPath, fromFileStats) {
+	if (third_party_files_from_last_run[toPath]) {
+		delete third_party_files_from_last_run[toPath];
+	}
+	third_party_files[toPath] = true;
+	createDirectoryIfItDoesntExist(path.dirname(toPath));
+
+	if (fs.existsSync(toPath)) {
+		const fromUpdateTime = fromFileStats.mtimeMs;
+		const toUpdateTime = fs.lstatSync(toPath).mtimeMs;
+
+		if (fromUpdateTime <= toUpdateTime) {
+			// File hasn't changed.
+			return;
+		}
+	}
+
+	console.log('Copying ' + toPath);
+	fs.copyFileSync(fromPath, toPath);
+}
+
+
+function copyFilesInDirectory(from, to, extension) {
+	const filesInDirectory = fs.readdirSync(from);
+	for (let i = 0; i < filesInDirectory.length; i++) {
+		const entryName = filesInDirectory[i];
+		const fromPath = from + '/' + entryName;
+		const toPath = to + '/' + entryName;
+
+		const fileStats = fs.lstatSync(fromPath);
+		if (!fileStats.isDirectory() &&
+			(extension == undefined || path.extname(entryName) == extension)) {
+			copyFile(fromPath, toPath, fileStats);
+		}
+	}
+}
+
+function replaceInFile(filename, needle, replaceWith) {
+	let fileContents = fs.readFileSync(filename, 'utf8');
+	fileContents = fileContents.split(needle).join(replaceWith);
+	fs.writeFileSync(filename, fileContents);
+}
+
+copyFilesInDirectory('third_party/extracted/dng_sdk_1_6/dng_sdk/source', 'source', '.cpp');
+copyFilesInDirectory('third_party/extracted/dng_sdk_1_6/dng_sdk/source', 'public', '.h');
+
+// Remove any third party files that don't exist in the latest build.
+Object.keys(third_party_files_from_last_run).forEach(function (filePath) {
+	if (fs.existsSync(filePath)) {
+		console.log('Removing ' + filePath);
+		fs.unlinkSync(filePath)
+		removeDirectoryIfEmpty(path.dirname(filePath));
+	};
+});
+fs.writeFileSync('third_party_files.json', JSON.stringify(third_party_files));
+
+
+
