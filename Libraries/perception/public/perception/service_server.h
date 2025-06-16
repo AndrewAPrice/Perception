fa233860bd40afe63cdef2e4481214fcef5d16d5
AndrewAPrice <291927+AndrewAPrice@users.noreply.github.com>
2025-06-16 18:09:38 -0400

    Rename RpcServer to ServiceServer and build out more.

---
+++ Libraries/perception/public/perception/service_server.h
@@ -0,0 +1,130 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <functional>
+#include <string_view>
+
+#include "perception/messages.h"
+#include "perception/rpc_memory.h"
+#include "perception/serialization/memory_read_stream.h"
+#include "perception/serialization/shared_memory_write_stream.h"
+#include "perception/shared_memory.h"
+#include "status.h"
+
+namespace perception {
+
+struct ServiceServerOptions {
+  bool is_public;
+};
+
+class ServiceServer {
+ public:
+  ProcessId ServerProcessId() const;
+
+  MessageId ServiceId() const;
+
+ protected:
+  ServiceServer(ServiceServerOptions options, std::string_view service_name);
+
+  virtual ~ServiceServer();
+
+  template <class ResponseType, class RequestType, class Service>
+  void HandleExpectedRequest(Service* service,
+                             ResponseType (Service::*handler)(RequestType&),
+                             ProcessId sender, const MessageData& message) {
+    RequestType request;
+
+    if (message.param3 == SIZE_MAX) {
+      // No attached message.
+      serialization::DeserializeToEmpty(request);
+    } else {
+      // Deserialized attached memory.
+      auto shared_memory =
+          GetMemoryBufferForReceivingFromProcess(sender, message.param3);
+      shared_memory->Grow(message.param4);
+      serialization::DeserializeFromSharedMemory(request, *shared_memory, 1);
+
+      SetMemoryBufferAsReadyForSendingNextMessageToProcess(*shared_memory);
+    }
+
+    if (message.param2 == SIZE_MAX) {
+      // Don't care about a response.
+      (void)(service->*handler)(request);
+      return;
+    }
+
+    auto response = (service->*handler)(request);
+    SendBackResponse(response, sender, message);
+  }
+
+  template <class ResponseType, class RequestType, class Service>
+  void HandleExpectedRequest(Service* service,
+                             ResponseType (Service::*handler)(),
+                             ProcessId sender, const MessageData& message) {
+    HandleUnexpectedMessageInRequest(sender, message);
+
+    if (message.param2 == SIZE_MAX) {
+      // Don't care about a response.
+      (void)(service->*handler)();
+      return;
+    }
+
+    auto response = (service->*handler)();
+    SendBackResponse(response, sender, message);
+  }
+
+  void HandleUnknownRequest(ProcessId sender, const MessageData& params);
+
+  virtual void HandleRequest(ProcessId sender, const MessageData& params) = 0;
+
+ private:
+  template <class ResponseType>
+  void SendBackResponse(ResponseType& response, ProcessId sender,
+                        const MessageData& message) {
+    MessageData response_data;
+    response_data.message_id = message.param2;
+    response_data.metadata = 0;
+    response_data.param1 =
+        static_cast<size_t>(::perception::ToStatus(response));
+    if constexpr (std::is_same_v<ResponseType, Status>) {
+      // Send back just the status.
+      response_data.param2 = SIZE_MAX;
+      response_data.param3 = 0;
+    } else {
+      if (response.Ok()) {
+        // Send back response type.
+        auto shared_memory = GetMemoryBufferForSendingToProcess(sender);
+        serialization::SerializeToSharedMemory(*response, *shared_memory, 1);
+        response_data.param2 = shared_memory->GetId();
+        response_data.param3 = shared_memory->GetSize();
+      } else {
+        // Send back non-ok status.
+        response_data.param2 = SIZE_MAX;
+        response_data.param3 = 0;
+        // TODO: Could send a status string in the remaining param bytes.
+      }
+    }
+    SendMessage(sender, response_data);
+  }
+
+  void HandleUnexpectedMessageInRequest(ProcessId sender,
+                                        const MessageData& message);
+
+  ServiceServerOptions options_;
+  MessageId message_id_;
+};
+
+}  // namespace perception
---
+++ Libraries/perception/source/perception/service_server.cc
@@ -0,0 +1,92 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "perception/service_server.h"
+
+#include <string_view>
+
+#include "perception/messages.h"
+#include "perception/processes.h"
+#include "perception/rpc_memory.h"
+#include "perception/services.h"
+
+namespace perception {
+
+// Implementation details:
+// Request:
+//    metadata:
+//    param 1: method ID
+//    param 2: message ID to use for response, or MAX if caller doesn't want a
+//    response
+//    param 3: shared buffer ID, or MAX for no attached messag
+//    param 4: length of message, in bytes (page aligned)
+//    param 5:
+//
+// Response:
+//    metadata:
+//    param 1: status
+//    param 2: shared buffer ID of response, or MAX for no attached method
+//    param 3: length of message, in byes (page aligned)
+//    param 4:
+//    param 5:
+//
+
+ServiceServer::ServiceServer(ServiceServerOptions options, std::string_view service_name)
+    : options_(options) {
+  message_id_ = GenerateUniqueMessageId();
+  RegisterRawMessageHandler(
+      message_id_, [this](::perception::ProcessId sender,
+                          const ::perception::MessageData& message_data) {
+        this->HandleRequest(sender, message_data);
+      });
+  if (options.is_public) {
+    RegisterService(message_id_, service_name);
+  }
+}
+
+ServiceServer::~ServiceServer() {
+  if (options_.is_public) {
+    UnregisterService(message_id_);
+  }
+  UnregisterMessageHandler(message_id_);
+}
+
+void ServiceServer::HandleUnknownRequest(ProcessId sender,
+                                     const MessageData& message) {
+  HandleUnexpectedMessageInRequest(sender, message);
+  if (message.param2 == SIZE_MAX) return;  // Dont care about a response.
+
+  // Send back an unimplemented status.
+  MessageData response_data;
+  response_data.message_id = message.param2;
+  response_data.metadata = 0;
+  response_data.param1 = static_cast<size_t>(Status::UNIMPLEMENTED);
+  response_data.param2 = SIZE_MAX;
+  response_data.param3 = 0;
+  SendMessage(sender, response_data);
+}
+
+ProcessId ServiceServer::ServerProcessId() const { GetProcessId(); }
+
+MessageId ServiceServer::ServiceId() const { return message_id_; }
+
+void ServiceServer::HandleUnexpectedMessageInRequest(ProcessId sender,
+                                                 const MessageData& message) {
+  if (message.param2 == SIZE_MAX) return;
+  auto shared_memory =
+      GetMemoryBufferForReceivingFromProcess(sender, message.param2);
+  SetMemoryBufferAsReadyForSendingNextMessageToProcess(*shared_memory);
+}
+
+}  // namespace perception
