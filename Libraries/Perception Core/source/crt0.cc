e6dcc5a86c99ef2ab40837304aeceed89de326c9
AndrewAPrice <291927+AndrewAPrice@users.noreply.github.com>
2026-06-19 07:15:01 -0400

    Bug fix

--- Libraries/Perception Core/source/crt0.cc
+++ Libraries/Perception Core/source/crt0.cc
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <stddef.h>
 #include <exception>
 
 // Some compilers mangle `main` (such as clang), others don't (such as GCC). To
