9548ac35c9bfda16d9c5f9f4be872fbd87eeedd9
AndrewAPrice <291927+AndrewAPrice@users.noreply.github.com>
2026-06-19 07:15:30 -0400

    Bug fix

--- Libraries/Perception Driver/source/perception/multiboot.cc
+++ Libraries/Perception Driver/source/perception/multiboot.cc
@@ -19,7 +19,7 @@
 namespace perception {
 
 // Maximum module name length.
-constexpr int kMaximumModuleNameLength = 11 * 8;
+constexpr int kMaximumModuleNameLength = 10 * 8;
 
 // Gets the details of the framebuffer set up by the bootloader.
 void GetMultibootFramebufferDetails(size_t& physical_address, uint32& width,
@@ -55,46 +55,43 @@ void GetMultibootFramebufferDetails(size_t& physical_address, uint32& width,
 std::unique_ptr<MultibootModule> GetMultibootModule() {
 #ifdef PERCEPTION
   volatile register size_t syscall asm("rdi") = 60;
-  size_t address_and_flags;
-  size_t size;
-  size_t name_words[11];
-  __asm__ __volatile__(
-#ifndef optimized_BUILD_
-      R"(
-    push %%rbp
-    syscall
-    mov %%rbp, %1
-    mov %%r8, %6
-    mov %%r9, %7
-    mov %%r10, %8
-    mov %%r12, %9
-    mov %%r13, %10
-    mov %%r14, %11
-    mov %%r15, %12
-    pop %%rbp
-  )"
-#else
-      R"(
-    syscall
-    mov %%r8, %6
-    mov %%r9, %7
-    mov %%r10, %8
-    mov %%r12, %9
-    mov %%r13, %10
-    mov %%r14, %11
-    mov %%r15, %12
-  )"
-#endif
-      : "=D"(address_and_flags), "=r"(size),
-        "=a"(name_words[0]), "=b"(name_words[1]), "=d"(name_words[2]),
-        "=S"(name_words[3]), "=r"(name_words[4]), "=r"(name_words[5]),
-        "=r"(name_words[6]), "=r"(name_words[7]), "=r"(name_words[8]),
-        "=r"(name_words[9]), "=r"(name_words[10])
-      : "D"(syscall)
-      : "rcx"
-  );
+  volatile register size_t address_and_flags_r asm("rdi");
+  volatile register size_t size_r asm("r15");
+  volatile register size_t name_word0 asm("rax");
+  volatile register size_t name_word1 asm("rbx");
+  volatile register size_t name_word2 asm("rdx");
+  volatile register size_t name_word3 asm("rsi");
+  volatile register size_t name_word4 asm("r8");
+  volatile register size_t name_word5 asm("r9");
+  volatile register size_t name_word6 asm("r10");
+  volatile register size_t name_word7 asm("r12");
+  volatile register size_t name_word8 asm("r13");
+  volatile register size_t name_word9 asm("r14");
 
+  __asm__ __volatile__("syscall\n"
+                       : "=r"(address_and_flags_r), "=r"(size_r),
+                         "=r"(name_word0), "=r"(name_word1), "=r"(name_word2),
+                         "=r"(name_word3), "=r"(name_word4), "=r"(name_word5),
+                         "=r"(name_word6), "=r"(name_word7), "=r"(name_word8),
+                         "=r"(name_word9)
+                       : "r"(syscall)
+                       : "rcx", "r11", "memory");
+
+  size_t size = size_r;
   if (size == 0) return nullptr;
+
+  size_t address_and_flags = address_and_flags_r;
+  size_t name_words[10];
+  name_words[0] = name_word0;
+  name_words[1] = name_word1;
+  name_words[2] = name_word2;
+  name_words[3] = name_word3;
+  name_words[4] = name_word4;
+  name_words[5] = name_word5;
+  name_words[6] = name_word6;
+  name_words[7] = name_word7;
+  name_words[8] = name_word8;
+  name_words[9] = name_word9;
 
   auto multiboot_module = std::make_unique<MultibootModule>();
   multiboot_module->flags = address_and_flags & (4096 - 1);
@@ -105,9 +102,8 @@ std::unique_ptr<MultibootModule> GetMultibootModule() {
   char module_name[kMaximumModuleNameLength + 1];
 
   // Copy the string out of the registers into a char array.
-  for (int i = 0; i < 11; ++i) {
-    ((size_t *)module_name)[i] = name_words[i];
-  }
+  for (int i = 0; i < 10; ++i) ((size_t*)module_name)[i] = name_words[i];
+
   module_name[kMaximumModuleNameLength] = '\0';
   multiboot_module->name = std::string(module_name);
 
