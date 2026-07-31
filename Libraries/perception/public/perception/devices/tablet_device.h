81c817694abcef084bc4aeccfaac93cd4a01466b
AndrewAPrice <291927+AndrewAPrice@users.noreply.github.com>
2026-07-31 15:21:56 -0400

    Define a tablet device API.

---
+++ Libraries/perception/public/perception/devices/tablet_device.h
@@ -0,0 +1,40 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "perception/devices/tablet_listener.h"
+#include "perception/serialization/serializable.h"
+#include "perception/service_macros.h"
+
+namespace perception {
+namespace devices {
+
+class MouseCaptureState : public serialization::Serializable {
+ public:
+  bool is_captured;
+
+  virtual void Serialize(serialization::Serializer& serializer) override;
+};
+
+#define METHOD_LIST(X)                                  \
+  X(1, SetTabletListener, void, TabletListener::Client) \
+  X(2, SetMouseCaptured, void, MouseCaptureState)
+
+DEFINE_PERCEPTION_SERVICE(TabletDevice, "perception.devices.TabletDevice",
+                          METHOD_LIST)
+#undef METHOD_LIST
+
+}  // namespace devices
+}  // namespace perception
